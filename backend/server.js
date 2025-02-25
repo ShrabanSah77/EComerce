cb3cfe417cb8ea45d6ff6f0acbeef14033240946
ShrabanSah77 <shrabanshah77@gmail.com>
2025-02-25 04:03:17 -0600

    create serverjs file

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require ("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require ("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// connect to MongoDB
+
+mongoose
+.connect(process.env.MONGO-URIError, {useNewUrlParser: true, useUnifiedTopology:true})
+.then(() => console.log("MongoDB Connected"))
+.catch((error) => console.log(erro));
+
+// Routes
+
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
