1ac6de77d0fc32a5e4079dead5fe427b8ce45bee
ShrabanSah77 <shrabanshah77@gmail.com>
2025-02-25 04:03:34 -0600

    create userjs file

---
+++ backend/models/user.js
@@ -0,0 +1,9 @@
+// Create a User model for MongoDB
+
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+    email:{type:String, required: true, unique: true},
+    password: { type: String, required: true },
+});
+
+module.exports = mongoose.model("User", UserSchema);
